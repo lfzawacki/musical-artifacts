220836b64efaf13e86b84bec3040cbd7acd73178
Lucas Zawacki <lfzawacki@gmail.com>
2015-04-26 21:19:28 -0300

    Overhaul css and about page

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,6 +1,8 @@
 class InfoController < ApplicationController
 
   def about
+    @tags = Artifact.tag_counts.order('taggings_count DESC').pluck(:name).first(8)
+
     render layout: false
   end
 end
