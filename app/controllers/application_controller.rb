02253cacb29d75832c305d74662e8d8e8825559f
Lucas Zawacki <lfzawacki@gmail.com>
2015-07-06 23:41:20 -0300

    Using settings for commenting system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :load_settings
+
+  def load_settings
+    @setting = Setting.first
+  end
+
 end
