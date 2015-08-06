f1950826b191094c3379d678dd51e8856594680e
Lucas Zawacki <lfzawacki@gmail.com>
2015-08-05 23:57:32 -0300

    Add some mime types

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,35 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+types = {
+  sfz: 'text/plain',
+  sf2: 'audio/x-soundfont',
+  sfark: 'application/x-extension-sfark',
+  gig: 'audio/x-riff',
+
+  midi: 'application/x-midi',
+  mid: 'application/x-midi',
+
+  xiz: 'application/gzip',
+  xmz: 'application/gzip',
+  xpf: 'text/html',
+
+  torrent: 'application/x-bittorrent',
+
+  h2song: 'application/xml',
+  h2drumkit: 'application/gzip',
+
+  # '7z' => 'application/x-7z-compressed',
+  rar: 'application/x-rar',
+  bz2: 'application/bzip2',
+  gz: 'application/x-gzip',
+  tar: 'application/x-tar',
+  tgz: 'application/x-gzip',
+
+  wav: 'audio/x-wav'
+}
+
+types.each do |type, str|
+  Mime::Type.register str, type.to_sym
+end
