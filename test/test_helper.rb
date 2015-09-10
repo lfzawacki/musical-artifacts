9a971b14d43f9e62edb592a4193c973cf727331b
Lucas Zawacki <lfzawacki@gmail.com>
2015-09-10 01:37:36 -0300

    Measure code test coverage (try 2)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,6 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
@@ -5,9 +8,6 @@ require 'minitest/reporters'
 require 'minitest/rails/capybara'
 require 'html_reporter'
 require 'forgery'
-
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
 
 reporter_options = { color: true }
 Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new, Minitest::Reporters::HtmlReporter.new]
