3f5ef1b091304928ec448326923e5521b8ba1f5e
Lucas Zawacki <lfzawacki@gmail.com>
2015-10-15 20:40:26 -0300

    Indicate page is loading with bar + spinner

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails', '~> 4.0.4'
 
 gem 'turbolinks'
+gem 'nprogress-rails'
+
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,17 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+
+//= require nprogress
+//= require nprogress-turbolinks
+
 //= require bootstrap
 //= require select2
+
 //= require_tree .
+
+Turbolinks.enableTransitionCache();
