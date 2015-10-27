e8ab9a20894f4f2b190d3992eb3d0d3a91b0f493
Lucas Zawacki <lfzawacki@gmail.com>
2015-10-27 00:01:53 -0200

    Use a default title on main index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def application_title
+    if content_for(:title).present?
+      "#{content_for :title } | #{@setting.site_name}"
+    else
+      "#{@setting.site_name} | #{I18n.t('layouts.application.default_title')}"
+    end
+  end
+
 end
