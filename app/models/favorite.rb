f37509243f7b6e7ba16359d00e0129357f2218f9
Lucas Zawacki <lfzawacki@gmail.com>
2016-02-01 00:34:30 -0500

    Favorite create/destroy activities

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -5,10 +5,23 @@ class Favorite < ActiveRecord::Base
   after_create :update_favorite_count
   after_destroy :update_favorite_count
 
+  after_create :create_activity
+  after_destroy :destroy_activity
+
   validates :artifact, presence: true, uniqueness: { scope: :user, allow_blank: false }
   validates :user, presence: true
 
   def update_favorite_count
     artifact.update_column :favorite_count, artifact.favorites.count
   end
+
+  private
+  def create_activity
+    artifact.create_activity key: 'artifact.favorite', owner: user
+  end
+
+  def destroy_activity
+    PublicActivity::Activity.where(key: 'artifact.favorite', owner: user, trackable: artifact).delete_all
+  end
+
 end
