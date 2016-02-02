b472ac8eb38d3316bb7e214319858a3a9eac0b08
Lucas Zawacki <lfzawacki@gmail.com>
2016-02-02 14:00:00 -0500

    Remove old tasks that added scraped content

--- lib/tasks/sf2.rb
+++
@@ -1,9 +0,0 @@
-Artifact.create(name: 'Wurlitzer Soundfont',
-tag_list: ['wurlitzer', 'soundfont'],
-software_list: ['linuxsampler', 'carla', 'fluidsynth', 'timidity'],
-more_info_urls: ['http://www.virtualorgan.com/Default.asp?page=58', 'http://www.virtualorgan.com/virtualorgan/downloads/inc_downloading.asp?iFile=205'],
-description: "If you are looking for Wurlitzer pipe organ samples for your Miditzer here they are. John Tay has has created a set of digital samples for the Miditzer™216. The instrument sampled is the Wurlitzer Theatre Organ at the Paramount Theatre in Middletown New York.  John generously offers these for download and only asks you to please make a small donation to the New York Theatre Organ Society.",
-file: File.open('./sf2/Wurlitzer.sf2'),
-author: 'John R. Tay',
-file_format: 'sf2',
-license_id: 9)
