ee9e94053f25a8568b12cd3b14634fed5e286b38
Lucas Zawacki <lfzawacki@gmail.com>
2024-06-03 20:50:54 -0300

    Adding newer options for admin dashboard

--- app/admin/artifact.rb
+++ app/admin/artifact.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Artifact do
-  permit_params :name, :approved, :downloadable, :author, :user, :file_hash, :mirrors, :license,
+  permit_params :name, :approved, :downloadable, :author, :user, :mirrors, :license,
     :more_info_urls, :extra_license_text, :stored_files, :user, :user_id
 
   index do
@@ -11,11 +11,18 @@ ActiveAdmin.register Artifact do
     column :software_list
     column :file_format_list
     column :created_at
+    column :approved
+    column :downloadable
     actions
   end
 
   filter :name
   filter :created_at
+  filter :approved
+  filter :downloadable
+  filter :user
+  filter :favorite_count
+  filter :download_count
 
   form do |f|
     f.inputs "Artifact Details" do
@@ -24,7 +31,6 @@ ActiveAdmin.register Artifact do
       f.input :approved
       f.input :downloadable
       f.input :author
-      f.input :file_hash
       f.input :mirrors, input_html: { value: f.artifact.mirrors.try(:join, ',') }
       f.input :more_info_urls, input_html: { value: f.artifact.more_info_urls.try(:join, ',') }
       f.input :license
