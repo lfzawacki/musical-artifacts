9b4a417fd8f9bfa1da5a123fb0c32edcaa6b77da
Lucas Zawacki <lfzawacki@gmail.com>
2026-06-13 11:39:33 -0300

    Properly cache tag queries in partials

--- app/controllers/artifacts_controller.rb
+++ app/controllers/artifacts_controller.rb
@@ -25,7 +25,7 @@ class ArtifactsController < InheritedResources::Base
 
   # Eager load associations only if the cache check misses
   before_filter only: [:index] do
-    @artifacts = @artifacts.includes(:license, :tags, :taggings, :stored_files)
+    @artifacts = @artifacts.includes(:license, :stored_files, :tags, :software, :file_formats)
   end
 
   before_action only: [:show] do
